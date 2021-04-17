69f0ada68c109352fe3fbb2fca17e82408f69dd1
Watotacho <watotacho@gmail.com>
2021-04-17 07:06:13 +0300

    first commit

---
+++ src/control.kt
@@ -0,0 +1,52 @@
+fun main(){
+number()
+   words("sarah","Waunita","wato","fatuma")
+   robot(5)
+   robot(12)
+   robot(23)
+   game()
+}
+fun number() {
+   for (num in 1..100) {
+      if (num % 2 != 0) {
+         println(num)
+      }
+   }
+}
+fun words(name1:String,name2:String,name3:String,name4:String):Array<String>{
+var names=arrayOf(name1,name2,name3,name4)
+   for (name in names)
+   if(name.length>5){
+     println(name)
+   }
+      return names
+
+   }
+fun robot(age:Int){
+   if(age<=6){
+      println("serve a glass of milk for the guest")
+   }
+   else if(age in 6..15){
+      println("serve a bottle of fanta orange for the guest")
+   }
+   else
+      println("serve a bottle of coca cola for the guest")
+}
+fun game(){
+   for (numbers in  1..100) {
+
+   if(numbers % 3 == 0 && numbers % 5 == 0) {
+      println("FizzBuzz")}
+      else if (numbers % 3 == 0) {
+         println("Fizz")
+      } else if (numbers % 5 == 0) {
+      println("Buzz")
+   }else if(numbers%3==0 &&numbers% 5 ==0){
+      println("FizzBuzz")
+   }else{
+   println(numbers)
+
+      }
+   }
+}
+
